0956c21f98e3a95429bcb44bfd5124a2aa8b2844
crispymccoy145 <samspam19941@gmail.com>
2021-04-09 11:28:01 +0100

    final final commit, completed the tests and the game logic

--- classes/game.py
+++ classes/game.py
@@ -8,9 +8,26 @@ class Game:
 
     #win_state() function takes in 2 gesture strings and returns a formatted string declaring the winner
     def win_state(self, player_1_gesture, player_2_gesture):
-        if player_1_gesture == "Scissors" and player_2_gesture == "Paper":
-            return "player 1 wins"
+        if player_1_gesture == player_2_gesture:
+            return "it's a draw"
+        elif player_1_gesture == "Scissors":
+            if player_2_gesture == "Paper":
+                return "player 1 wins"
+            elif player_2_gesture == "Rock":
+                return "player 2 wins"
+        elif player_1_gesture == "Rock":
+            if player_2_gesture == "Scissors":
+                return "player 1 wins"
+            elif player_2_gesture == "Paper":
+                return "player 2 wins"
+        elif player_1_gesture == "Paper":
+            if player_2_gesture == "Rock":
+                return "player 1 wins"
+            elif player_2_gesture == "Scissors":
+                return "player 2 wins"
+        
+        
 
-    # needs if statements for every outcome but we ran out of time    
+
 
     
--- tests/test_game.py
+++ tests/test_game.py
@@ -16,11 +16,59 @@ class TestGame(unittest.TestCase):
     def test_game_has_player_2(self):
         self.assertEqual(self.player_2, self.game_state.player_2)
     
-    def test_scissors_beats_paper(self):
+    def test_scissors_beats_paper_player_1_wins(self):
         player_1_gesture = self.player_1.return_gesture(2)
         player_2_gesture = self.player_2.return_gesture(1)
         
         self.assertEqual("player 1 wins", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_paper_beats_rock_player_1_wins(self):
+        player_1_gesture = self.player_1.return_gesture(1)
+        player_2_gesture = self.player_2.return_gesture(0)
+        
+        self.assertEqual("player 1 wins", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_rock_beats_scissors_player_1_wins(self):
+        player_1_gesture = self.player_1.return_gesture(0)
+        player_2_gesture = self.player_2.return_gesture(2)
+        
+        self.assertEqual("player 1 wins", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_scissors_beats_paper_player_2_wins(self):
+        player_1_gesture = self.player_1.return_gesture(1)
+        player_2_gesture = self.player_2.return_gesture(2)
+        
+        self.assertEqual("player 2 wins", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_paper_beats_rock_player_2_wins(self):
+        player_1_gesture = self.player_1.return_gesture(0)
+        player_2_gesture = self.player_2.return_gesture(1)
+        
+        self.assertEqual("player 2 wins", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_rock_beats_scissors_player_2_wins(self):
+        player_1_gesture = self.player_1.return_gesture(2)
+        player_2_gesture = self.player_2.return_gesture(0)
+        
+        self.assertEqual("player 2 wins", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_rock_draw(self):
+        player_1_gesture = self.player_1.return_gesture(0)
+        player_2_gesture = self.player_2.return_gesture(0)
+        
+        self.assertEqual("it's a draw", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_paper_draw(self):
+        player_1_gesture = self.player_1.return_gesture(1)
+        player_2_gesture = self.player_2.return_gesture(1)
+        
+        self.assertEqual("it's a draw", self.game_state.win_state(player_1_gesture,player_2_gesture))
+    
+    def test_scissors_draw(self):
+        player_1_gesture = self.player_1.return_gesture(2)
+        player_2_gesture = self.player_2.return_gesture(2)
+        
+        self.assertEqual("it's a draw", self.game_state.win_state(player_1_gesture,player_2_gesture))
 
 
     
